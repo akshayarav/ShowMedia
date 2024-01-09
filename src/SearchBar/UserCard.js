ffda9b742c63bcd92968655ce87d47c1ebf20b6a
Akshay Aravind <akshayarav2013@gmail.com>
2024-01-09 14:55:40 -0500

    User search now triggers a reload

--- src/SearchBar/UserCard.js
+++ src/SearchBar/UserCard.js
@@ -6,12 +6,12 @@ import FollowButton from '../Profile/Overview/FollowButton';
 function UserCard({ other_user }) {
     return (
         <div className="p-3 border-bottom d-flex text-dark text-decoration-none account-item">
-            <Link to={`/profile/${other_user.username}`} className="text-decoration-none">
+            <Link to={`/profile/${other_user.username}`} className="text-decoration-none" onClick={() => window.location.href = `/profile/${other_user.username}`}>
                 <img src={other_user.profilePicture} className="img-fluid rounded-circle me-3" alt="profile-img" />
             </Link>
-            <Link to={`/profile/${other_user.username}`} className="text-decoration-none">
+            <Link to={`/profile/${other_user.username}`} className="text-decoration-none"  onClick={() => window.location.href = `/profile/${other_user.username}`}>
                 <div>
-                    <p className="fw-bold mb-0 pe-3 d-flex align-items-center text-decoration-none text-dark">{other_user.username}</p>
+                    <p className="fw-bold mb-0 pe-3 d-flex align-items-center text-decoration-none text-dark">{other_user.username} </p>
                 </div>
             </Link>
             <FollowButton other_user={other_user} />
