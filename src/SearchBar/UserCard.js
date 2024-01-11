64b5bb56f5e496465c5b751ae36b9386d8122588
Akshay Aravind <akshayarav2013@gmail.com>
2024-01-11 15:45:55 -0500

    Implement borders on all pages

--- src/SearchBar/UserCard.js
+++ src/SearchBar/UserCard.js
@@ -16,7 +16,7 @@ function UserCard({ other_user, toggleRefresh }) {
             <Link to={`/profile/${other_user.username}`} className="stretched-link" onClick = {handleLinkClick}/>
             <img src={other_user.profilePicture} className="img-fluid rounded-circle me-3" alt="profile-img" />
             <div className="user-info flex-grow-1">
-                <p className="fw-bold mb-0 text-light">{other_user.username}</p>
+                <p className="fw-bold mb-0 text-light">@{other_user.username}</p>
                 <small className="text-muted">{other_user.first} {other_user.last}</small>
             </div>
             <div className="ms-auto">
