50759e937f8445067bb316a7843d1149569f970a
Akshay Aravind <akshayarav2013@gmail.com>
2024-01-11 15:00:01 -0500

    Add loading animation to feed

--- src/Feed/feed.js
+++ src/Feed/feed.js
@@ -15,11 +15,12 @@ function Feed() {
     const { isAuthenticated } = useContext(AuthContext);
     const [searchScreenOn, setSearchScreenOn] = useState(false)
     const [searchResults, setSearchResults] = useState([]);
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true);
         const userId = localStorage.getItem('userId');
         if (isAuthenticated && userId) {
-            // Fetch following feed using the user ID
             axios.get(`${apiUrl}/api/followingFeed/${userId}`)
                 .then(response => {
                     setActivities(response.data);
@@ -29,6 +30,13 @@ function Feed() {
                 });
         }
     }, [isAuthenticated, apiUrl, feedKey]);
+
+    useEffect(() => {
+        if (activities) {
+            setIsLoading(false);
+        }
+    }, [activities]);
+
 
     const handleFeedTabClick = () => {
         setFeedKey(prevKey => prevKey + 1);
@@ -45,6 +53,54 @@ function Feed() {
         </div>)
     }
 
+    if (isLoading) {
+        return (
+            <div className="bg-brown-gradient">
+                <MobileBar toggleOffcanvas={() => setIsOffcanvasOpen(!isOffcanvasOpen)} toggleSearchScreen={(e) => setSearchScreenOn(e)} setSearchResults={(e) => setSearchResults(e)} />
+                <div className="py-4">
+                    <div className="container">
+                        <div className="row position-relative">
+                            <main className="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+                                <div className="main-content">
+                                    <h2 class="fw-bold text-white mt-4">Your Feed</h2>
+                                    <div class="text-center" style={{ marginTop: '70px' }}>
+                                        <div class="spinner-border" role="status" style={{ width: '3rem', height: '3rem' }}>
+                                            <span class="visually-hidden">Loading...</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </main>
+                            <Sidebar isOffcanvasOpen={isOffcanvasOpen} toggleOffcanvas={handleFeedTabClick} />
+                            <SearchBar />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    if (!isLoading && activities && activities.length === 0) {
+        return (
+            <div className="bg-brown-gradient">
+                <MobileBar toggleOffcanvas={() => setIsOffcanvasOpen(!isOffcanvasOpen)} toggleSearchScreen={(e) => setSearchScreenOn(e)} setSearchResults={(e) => setSearchResults(e)} />
+                <div className="py-4">
+                    <div className="container">
+                        <div className="row position-relative">
+                            <main className="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
+                                <div className="main-content">
+                                    <h2 className="fw-bold text-white mt-4">Your Feed</h2>
+                                    <p>No following activity to display.</p>
+                                </div>
+                            </main>
+                            <Sidebar isOffcanvasOpen={isOffcanvasOpen} toggleOffcanvas={handleFeedTabClick} />
+                            <SearchBar />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="bg-brown-gradient">
             <MobileBar toggleOffcanvas={() => setIsOffcanvasOpen(!isOffcanvasOpen)} toggleSearchScreen={(e) => setSearchScreenOn(e)} setSearchResults={(e) => setSearchResults(e)} />
@@ -54,11 +110,9 @@ function Feed() {
                         <main className="col col-xl-6 order-xl-2 col-lg-12 order-lg-1 col-md-12 col-sm-12 col-12">
                             <div className="main-content">
                                 <h2 class="fw-bold text-white mt-4">Your Feed</h2>
-                                {isAuthenticated && activities && activities.length > 0 ? (
+                                {isAuthenticated && activities && activities.length > 0 && (
                                     <FollowingFeed activities={activities} />
-                                ) : (
-                                    <p>No following activity to display.</p>
-                                )}
+                                ) }
                                 {!isAuthenticated && <p>Please log in to see the feed.</p>}
                             </div>
                         </main>
