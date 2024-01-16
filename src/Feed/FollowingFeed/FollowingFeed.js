35b0746e327eaee66d8092633ad803dde12a4c2d
Dan Wei Zuo <dz267@cornell.edu>
2024-01-15 23:32:13 -0500

    Comments in following feed refresh on submit

--- src/Feed/FollowingFeed/FollowingFeed.js
+++ src/Feed/FollowingFeed/FollowingFeed.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import FeedItem from '../../Common/FeedItem';
 
-function FollowingFeed({ activities, toggleRefresh }) {
+function FollowingFeed({ activities, refresh, toggleRefresh  }) {
     return (
         <div>
             {activities.map((activity, index) => (
-                <FeedItem key={index} activity={activity} toggleRefresh = {toggleRefresh}/>
+                <FeedItem key={index} activity={activity} refresh={refresh} toggleRefresh={toggleRefresh}/>
             ))}
         </div>
     );
