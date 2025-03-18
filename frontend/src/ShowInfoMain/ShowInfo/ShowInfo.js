39e5176d1976f67ecda33eb025bea9ab6f2fe535
Akshay Aravind <akshay@Mac.lan>
2025-03-18 17:44:42 -0400

    ShowInfo now modified for mobile view

--- frontend/src/ShowInfoMain/ShowInfo/ShowInfo.js
+++ frontend/src/ShowInfoMain/ShowInfo/ShowInfo.js
@@ -23,7 +23,7 @@ function ShowInfo() {
   const formatDate = (dateString) => {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
     return new Date(dateString).toLocaleDateString(undefined, options);
-};
+  };
 
   const [showModal, setShowModal] = useState(false);
   const toggleModal = () => setShowModal(!showModal)
@@ -54,12 +54,12 @@ function ShowInfo() {
   }, [apiUrl, userId]);
 
   useEffect(() => {
-    if (recShows) {
+    if (recShows && show) {
       show.users = recShows.has(show.id.toString())
         ? recShows.get(show.id.toString()).users
         : [];
     }
-  }, [recShows]);
+  }, [recShows, show]);
 
   async function fetchShowDetails(showId) {
     setIsLoading(true);
@@ -153,10 +153,12 @@ function ShowInfo() {
                     <div>{show?.overview}</div>
                   </div>
                   <div className="row bg-glass p-3 feed-item rounded-4 shadow-sm z-top">
-                    <div className="col-3">
+                    {/* Modified layout for better responsive behavior */}
+                    <div className="col-md-3 col-sm-12 mb-3 mb-md-0">
                       <img
                         src={`https://image.tmdb.org/t/p/w500${show?.poster_path}`}
-                        style={{ maxWidth: "100%", height: "auto" }}
+                        className="img-fluid mx-auto d-block"
+                        alt={show?.name}
                       />
                       <div className="d-flex mb-3 mt-3 justify-content-center">
                         <AddToListButton show={show} openModal={toggleModal} />
@@ -165,138 +167,116 @@ function ShowInfo() {
                         showModal && (
                           <ShowModal
                             closeModal={() => setShowModal(false)}
-                            showName={show.name}
+                            showName={show?.name}
                             showImg={`https://image.tmdb.org/t/p/w500${show?.poster_path}`}
-                            series_id={show.id}
+                            series_id={show?.id}
                             seasons={seasons}
                           />
                         )}
                     </div>
-                    <div
-                      className="py-3 px-lg-3 col-9 d-flex flex-column"
-                      style={{ maxHeight: "345px", gap: "10px" }}
-                    >
+                    <div className="py-3 px-lg-3 col-md-9 col-sm-12 d-flex flex-column">
                       <div className="container">
-                        <div className="row ">
+                        <div className="row">
                           <h6 className="fw-bold text-body text-center border-bottom pb-2">
-                            {" "}
                             Show Information
                           </h6>
                         </div>
 
+                        {/* First row of info - responsive grid */}
                         <div className="row d-flex justify-content-center border-bottom p-2">
-                          <div className="bg-brown-gradient-color rounded-4 col-2 p-3">
-                            <h6 className="fw-bold text-primary text-center mt-2">
+                          <div className="bg-brown-gradient-color rounded-4 col-6 col-md-2 p-2 p-md-3 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
                               Status
                             </h6>
-                            <div className="text-center"> {show?.status}</div>
+                            <div className="text-center small">{show?.status}</div>
                           </div>
-                          <div className="bg-brown-gradient-color rounded-4 p-3 col-6 m-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
+                          <div className="bg-brown-gradient-color rounded-4 p-2 p-md-3 col-10 col-md-6 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
                               Genres
                             </h6>
-                            <div className="text-center">
+                            <div className="text-center small">
                               {show?.genres
                                 ?.map((genre) => genre.name)
                                 .join(", ")}
                             </div>
                           </div>
-                          <div className="bg-brown-gradient-color rounded-4 p-3 col-2 ms-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
-                              First Air Date
+                          <div className="bg-brown-gradient-color rounded-4 p-2 p-md-3 col-6 col-md-2 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
+                              First Air
                             </h6>
-                            <div className="text-center">
-                              {" "}
-                              {formatDate(show?.first_air_date)}
+                            <div className="text-center small">
+                              {show?.first_air_date ? formatDate(show.first_air_date) : 'N/A'}
                             </div>
                           </div>
                         </div>
 
+                        {/* Second row of info - responsive grid with smaller cards */}
                         <div className="row d-flex justify-content-center border-bottom p-2">
-                          <div className="bg-brown-gradient-color rounded-4 p-3 col-2 m-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
-                              Vote Average
+                          <div className="bg-brown-gradient-color rounded-4 p-2 p-md-3 col-5 col-md-2 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
+                              Rating
                             </h6>
-                            <div className="text-center"> {show?.vote_average}</div>
+                            <div className="text-center small">{show?.vote_average}</div>
                           </div>
-                          <div className="bg-brown-gradient-color rounded-4 col-2 p-3 m-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
-                              Vote Count
+                          <div className="bg-brown-gradient-color rounded-4 col-5 col-md-2 p-2 p-md-3 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
+                              Votes
                             </h6>
-                            <div className="text-center"> {show?.vote_count}</div>
+                            <div className="text-center small">{show?.vote_count}</div>
                           </div>
-                          <div className="bg-brown-gradient-color rounded-4 col-2 p-3 m-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
-                              Number of Episodes
+                          <div className="bg-brown-gradient-color rounded-4 col-5 col-md-2 p-2 p-md-3 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
+                              Episodes
                             </h6>
-                            <div className="text-center">
-                              {" "}
+                            <div className="text-center small">
                               {show?.number_of_episodes}
                             </div>
                           </div>
-                          <div className="bg-brown-gradient-color rounded-4 col-2 p-3 m-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
-                              Number of Seasons
+                          <div className="bg-brown-gradient-color rounded-4 col-5 col-md-2 p-2 p-md-3 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
+                              Seasons
                             </h6>
-                            <div className="text-center">
-                              {" "}
+                            <div className="text-center small">
                               {show?.number_of_seasons}
                             </div>
                           </div>
-                          <div className="bg-brown-gradient-color rounded-4 col-2 p-3 m-1">
-                            <h6 className="fw-bold text-primary text-center mt-2">
+                          <div className="bg-brown-gradient-color rounded-4 col-5 col-md-2 p-2 p-md-3 m-1">
+                            <h6 className="fw-bold text-primary text-center mt-2 fs-6">
                               Popularity
                             </h6>
-                            <div className="text-center">
-                              {" "}
+                            <div className="text-center small">
                               {show?.popularity}
                             </div>
-                          </div>
-                        </div>
-
-                        <div className="row">
-                          <div className="d-flex justify-content-center">
-                            {(
-                              show?.networks?.map((network, index) => (  // Include 'index' here
-                                <img src={
-                                  network.logo_path
-                                    ? `https://image.tmdb.org/t/p/w45/${network.logo_path}`
-                                    : "/error.jpg"
-                                }
-                                className = "m-3">
-                                </img>
-                              ))
-                            )}
                           </div>
                         </div>
                       </div>
                     </div>
                   </div>
-                  <div className="row border-top mt-3">
-                    <div className="d-flex p-3 flex-grow-1">
-                      {show &&
-                        show.created_by &&
-                        show.created_by.length > 0 &&
+                  
+                  {/* Created By section */}
+                  {show && show.created_by && show.created_by.length > 0 && (
+                    <div className="row border-top mt-3">
+                      <div className="d-flex p-3 flex-grow-1">
                         <div className="bg-glass rounded-4 flex-grow-1">
                           <h5 className="fw-bold text-body p-3 mb-0 d-flex justify-content-center">
                             Created By
                           </h5>
-                          <div className="d-flex overflow-auto justify-content-center">
-                            {(
-                              show.created_by.map((creator, index) => (  // Include 'index' here
-                                <div key={index} className="d-flex fw-bold text-center text-primary justify-content-center me-2">
-                                  <p>{creator.name}</p>
-                                  {index !== show.created_by.length - 1 && (
-                                    <span className="material-icons md-10 m-1 text-white">circle</span>
-                                  )}
-                                </div>
-                              ))
-                            )}
+                          <div className="d-flex flex-wrap overflow-auto justify-content-center">
+                            {show.created_by.map((creator, index) => (
+                              <div key={index} className="d-flex fw-bold text-center text-primary justify-content-center me-2">
+                                <p>{creator.name}</p>
+                                {index !== show.created_by.length - 1 && (
+                                  <span className="material-icons md-10 m-1 text-white">circle</span>
+                                )}
+                              </div>
+                            ))}
                           </div>
                         </div>
-                      }
+                      </div>
                     </div>
-                  </div>
+                  )}
+                  
+                  {/* Seen By section */}
                   <div className="row border-top mt-3">
                     <div className="p-3 scrollable-div">
                       <div className="bg-glass rounded-4 overflow-hidden shadow-sm account-follow mb-4">
@@ -331,7 +311,7 @@ function ShowInfo() {
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 }
 
